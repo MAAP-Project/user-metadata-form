d784288e1e7bef73a086030b51d58f5fd220b21d
Aimee Barciauskas <aimee@developmentseed.org>
2019-08-15 15:45:21 -0400

    Fix cumulus api requests

--- lib/cumulus_api.rb
+++ lib/cumulus_api.rb
@@ -1,19 +1,29 @@
 class CumulusApi
   def self.create_cumulus_collection(collection_data)
     token = generate_token
-    return HTTParty.post(
-      collections_url,
-      body: cumulus_collection(collection_data).to_json,
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': "Bearer #{token}"
-      }
+    headers = {
+      'Content-Type': 'application/json',
+      'Authorization': "Bearer #{token}"
+    }
+    body = cumulus_collection(collection_data).to_json
+    response = HTTParty.put(
+      "#{collections_url}/#{short_name(collection_data)}/#{collection_data.version}",
+      body: body,
+      headers: headers
     )
+    if response.code === 400 and response.parsed_response['message'] === 'Record does not exist'
+      response = HTTParty.post(
+        collections_url,
+        body: body,
+        headers: headers
+      )
+    end
+    response
   end
 
   def self.cumulus_collection(collection_data)
     return {
-      version: '0',
+      version: collection_data.version,
       files: [
         {
           regex: '^(.*\\.\\w{1,})$', # make sure it has some suffix
@@ -21,15 +31,19 @@ class CumulusApi
           bucket: 'internal'
         }
       ],
-      name: collection_data.title,
+      name: short_name(collection_data),
       sampleFileName: 'test.xyz',
       granuleIdExtraction: '^(.+)$',
       granuleId: '^.+$',
-      dataType: collection_data.title,
+      dataType: short_name(collection_data),
       provider_path: '',
       userAdded: true,
       jobIds: collection_data.job_ids
     }
+  end
+
+  def self.short_name(collection_data)
+    collection_data.short_title.gsub(' ', '_')
   end
 
   def self.generate_token
