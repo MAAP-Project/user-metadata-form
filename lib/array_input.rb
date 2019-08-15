4eb4ff9ffbe0275358864d27b0be25616fb4eaf8
Aimee Barciauskas <aimee@developmentseed.org>
2019-08-15 15:44:44 -0400

    Smart default for array_input

--- lib/array_input.rb
+++ lib/array_input.rb
@@ -2,7 +2,11 @@ class ArrayInput < SimpleForm::Inputs::StringInput
   def input(wrapper_options)
     input_html_options[:type] ||= input_type
 
-    Array(object.public_send(attribute_name)).map do |array_el|
+    array_elements = object.public_send(attribute_name)
+    if array_elements.length == 0
+      array_elements = ["", "", ""]
+    end
+    Array(array_elements).map do |array_el|
       @builder.text_field(nil, input_html_options.merge(value: array_el, name: "#{object_name}[#{attribute_name}][]"))
     end.join.html_safe
   end
