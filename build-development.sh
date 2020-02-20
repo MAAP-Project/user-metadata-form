1e8dcc1a53c9a3afae100b6daed1ef0a067edaca
Aimee Barciauskas <aimee@developmentseed.org>
2020-02-19 20:53:05 -0800

    Add earthdata credentials to build-development.sh

--- build-development.sh
+++ build-development.sh
@@ -2,4 +2,6 @@ docker-compose build \
   --build-arg ENV=development \
   --build-arg SECRET_KEY_BASE= \
   --build-arg DATABASE_PASSWORD= \
-  --build-arg DATABASE_HOST= 
+  --build-arg DATABASE_HOST= \
+  --build-arg EARTHDATA_USERNAME= \
+  --build-arg EARTHDATA_PASSWORD
