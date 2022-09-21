ea1dd4a70ae3efd6e0d1d562972cc7de4fd61ba2
Aimee Barciauskas <aimee@developmentseed.org>
2022-09-21 16:50:13 -0400

    Modify migration

---
+++ db/migrate/20220921174034_add_bucket_info_to_collection.rb
@@ -0,0 +1,8 @@
+class AddBucketInfoToCollection < ActiveRecord::Migration[5.2]
+  def change
+    add_column :collection_infos, :bucket, :string
+    add_column :collection_infos, :path, :string
+    add_column :collection_infos, :filename_prefix, :string
+    add_column :collection_infos, :filename_regex, :string
+  end
+end
